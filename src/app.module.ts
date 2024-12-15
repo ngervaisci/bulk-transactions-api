f65c7d1ca6410870731dc1ea535fa71552b5e358
Nathan Gervais <122788806+ngervaisci@users.noreply.github.com>
2024-12-15 17:44:23 +0000

    refactor: rempved centralised app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TransactionsModule } from './transactions/transactions.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [TransactionsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TransactionsModule, AccountsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
 
--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
 import { MockAccountRepository } from '../infrastructure/repositories/mock-account.repository';
-import { AccountsController } from '../accounts/accounts.controller';
 import { AccountsModule } from '../accounts/accounts.module';
 
 @Module({
